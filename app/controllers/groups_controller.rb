da006ef0491e77c02543739fec0d9cb4d9d35993
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-08 17:26:08 -0700

    adding some geo stuff, testing it

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   def index
+    @location = GeoIPClient.city(request.headers['REMOTE_HOST'])
     @groups = Group.find(:all)
   end
 end
--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
+require 'flexmock/test_unit'
 
 class GroupsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "get index with IP address" do
+    ip = '66.235.6.100'
+
+    @request.env['REMOTE_HOST'] = ip
+
+    flexmock(GeoIPClient).should_receive(:city).once.with(ip).and_return(
+      [ "66.235.6.100",
+        "66.235.6.100",
+        "US",
+        "USA",
+        "United States",
+        "NA",
+        "WA",
+        "Seattle",
+        "98144",
+        47.5839, -122.2995, 819, 206]
+    )
+    get :index
   end
 end
