197f0b85607e1b62987df362239f6214e25221c5
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-08 17:02:36 -0700

    adding groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
+  map.resources :groups
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
 
