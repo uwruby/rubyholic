21391a93c3589548838972caf7fab2eeaebf1018
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-09 21:32:04 -0700

    adding sphinx plugin

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,10 @@
 class Group < ActiveRecord::Base
   has_many :events
   has_many :locations, :through => :events
+
+  define_index do
+    indexes name
+    indexes events.description, :as => :event_descriptions
+    indexes [locations.name, locations.address], :as => :location_addresses
+  end
 end
