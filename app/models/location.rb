8aeacc9528ea36a5c6586a711313d21d1f4c99fe
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-09 21:15:00 -0700

    adding geokit mappable stuff

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
+  acts_as_mappable  :auto_geocode     => true,
+                    :lat_column_name  => 'latitude',
+                    :lng_column_name  => 'longitude'
+
   has_many :events
   has_many :groups, :through => :events
 end
